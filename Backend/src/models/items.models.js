4f73f4767e16e9afe640540c5f878067605cd893
Aryan <aryankawale163@gmail.com>
2026-06-04 23:35:12 +0530

    first commit

---
+++ Backend/src/models/items.models.js
@@ -0,0 +1,45 @@
+import mongoose from 'mongoose'
+
+const itemSchema = new mongoose.Schema({
+    userId:{
+        type:mongoose.Schema.types.ObjectId,
+        ref:"User",
+        required:true,
+        index:true
+    },
+    name:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    brand:{
+        type:String,
+        trim:true
+    },
+    category:{
+        type:String,
+        required:true
+    },
+    purchaseDate:{
+        type:Date,
+        required:true
+    },
+    warrantyMonths:{
+        type:Number,
+        required:true,
+        min:0
+    },
+    warrentyEndData:{
+        type:Date,
+        required:true
+    },
+    receiptUrl:{
+        type:String,
+    },
+    notes:{
+        type:String,
+        trim:true
+    }
+},{timestamp:true})
+
+const Item = mongoose.model('Item', itemSchema)
---
+++ Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [name,setName] = useState('')
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
